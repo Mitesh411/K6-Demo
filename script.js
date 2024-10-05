5e8b31795c4d85639b534736ba61dc8e95dc7313
Mitesh Dandade <miteshdandade@ymail.com>
2024-10-05 13:25:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 // This will export to HTML as filename "result.html" AND also stdout using the text summary json=results.json
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
 import { check, sleep } from 'k6';
 
 export const options ={
