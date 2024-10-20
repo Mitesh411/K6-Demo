f3e15a58e07ebae1f28ee215de37f4b919b18fea
Mitesh Dandade <miteshdandade@ymail.com>
2024-10-20 10:40:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 import http from 'k6/http';
-// This will export to HTML as filename "result.html" AND also stdout using the text summary json=results.json
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
-import { check, sleep } from 'k6';
 
-export const options ={
-    vus: 5,
-    duration: '3s'
+export const options = {
+  iterations: 1,
+};
+
+export default function () {
+  const response = http.get('https://test-api.k6.io/public/crocodiles/');
 }
-  export default function () {
-    const res =http.get('https://mitesh411.github.io/MyResume/');
-    check(res,{'Status was 200': r => r.status == 200});
-    sleep(1);
-  }
-
-  export function handleSummary(data){
-    return {
-      'index.html': htmlReporter(data),
-      stdout: textSummary(data, { indent: " ", enableColors: true }),
-    }
-  }
